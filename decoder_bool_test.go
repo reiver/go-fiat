488c124a8c6727747e925e485d08918325bef871
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-30 12:51:55 -0800

    decoder tests for decoding to bool

---
+++ decoder_bool_test.go
@@ -0,0 +1,43 @@
+package fiat
+
+import (
+	"testing"
+)
+
+func TestDecoderBoolFromUint8(t *testing.T) {
+
+	tests := []struct{
+		Value bool
+	}{
+		{
+			Value: false,
+		},
+		{
+			Value: true,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var decoder Decoder
+
+		const targetKey = "target_key"
+
+		decoder.data = map[string][]interface{}{}
+		decoder.data[targetKey] = []interface{}{ test.Value }
+
+		x, err := decoder.Bool(targetKey)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		y := bool(x)
+
+		if expected, actual := test.Value, y; expected != actual {
+			t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+			continue
+		}
+	}
+}
